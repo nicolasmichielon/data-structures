e975c481dd2745e8b9a9a71711bc15cf759ffcee
Nickkcj <nickcj123@hotmail.com>
2025-03-19 21:00:44 -0300

    feat: implement LinkedList data structure

---
+++ DataStructures/linked_list.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add(self, data):
+        if self.head is None:
+            self.head = Node(data)
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = Node(data)
+
+    def print(self):
+        current = self.head
+        while current is not None:
+            print(current.data, end=" ")
+            current = current.next
+        print()
